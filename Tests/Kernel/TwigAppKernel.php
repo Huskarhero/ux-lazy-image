6ad77d5cf6a0d88f9aa1e6eeb7091e901e4aeee1
Huskarhero <stevenjobs207@gmail.com>
2020-12-05 22:16:02 +0100

    Setup ESLint, and lint/format files

--- Tests/Kernel/TwigAppKernel.php
+++ Tests/Kernel/TwigAppKernel.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Tests\Symfony\UX\LazyImage\Kernel;
+namespace Symfony\UX\LazyImage\Tests\Kernel;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
@@ -35,8 +35,8 @@ class TwigAppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(function (ContainerBuilder $container) {
-            $container->loadFromExtension('framework', ['secret' => '$ecret']);
-            $container->loadFromExtension('twig', ['default_path' => __DIR__.'/templates']);
+            $container->loadFromExtension('framework', ['secret' => '$ecret', 'test' => true]);
+            $container->loadFromExtension('twig', ['default_path' => __DIR__.'/templates', 'strict_variables' => true, 'exception_controller' => null]);
 
             $container->setAlias('test.lazy_image.blur_hash', 'lazy_image.blur_hash')->setPublic(true);
         });
